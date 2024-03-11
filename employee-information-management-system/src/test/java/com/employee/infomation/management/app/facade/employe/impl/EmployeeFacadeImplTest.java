63a91a4cec9e357a51c990ef9fe563a9b119d5fe
unknown <julybayot26>
2024-03-12 01:19:39 +0800

    updated employee

--- employee-information-management-system/src/test/java/com/employee/infomation/management/app/facade/employe/impl/EmployeeFacadeImplTest.java
+++ employee-information-management-system/src/test/java/com/employee/infomation/management/app/facade/employe/impl/EmployeeFacadeImplTest.java
@@ -1,4 +1,54 @@
 package com.employee.infomation.management.app.facade.employe.impl;
 
-public class EmployeeFacadeImplTest {
+
+import com.employee.information.management.app.facade.employee.impl.EmployeeFacadeImpl;
+import com.employee.information.management.app.model.Employee;
+import com.employee.information.management.data.employee.dao.EmployeeDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.management.Query.times;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeFacadeImplTest {
+    private EmployeeDao employeeDao;
+
+    private EmployeeFacadeImpl employeeFacade;
+
+    @BeforeEach
+    public void setUp() {
+        employeeDao = mock(EmployeeDao.class);
+        employeeFacade = new EmployeeFacadeImpl();
+    }
+
+
+
+    @Test
+    public void testGetAllEmployee() {
+        List<Employee> EmployeeList = new ArrayList<>();
+        EmployeeList.add(new Employee());
+        EmployeeList.add(new Employee());
+        when(employeeDao.getAllEmployee()).thenReturn(EmployeeList);
+
+        List<Employee> retrievedEmployeeList = employeeFacade.getAllEmployees();
+
+        verify(employeeDao, times(1).getAllEmployee();
+        assertEquals(EmployeeList, retrievedEmployeeList);
+    }
+
+    @Test
+    void testGetEmployeeByNo() {
+        String employeeNo = "1";
+        Employee employee = new Employee();
+        when(employeeDao.getEmployeeByNo(employeeNo)).thenReturn(employee);
+
+        Employee retrievedEmployee = employeeFacade.getEmployeeByNo(employeeNo);
+
+        verify(employeeDao, times(1)).getEmployeeByNo(employeeNo);
+        assertEquals(employee, retrievedEmployee);
+    }
+
 }
--- employee-information-management-system/src/test/java/com/employee/infomation/management/data/employee/dao/impl/EmployeeDaoImplTest.java
+++ employee-information-management-system/src/test/java/com/employee/infomation/management/data/employee/dao/impl/EmployeeDaoImplTest.java
@@ -1,4 +1,62 @@
 package com.employee.infomation.management.data.employee.dao.impl;
 
-public class EmployeeDaoImplTest {
+
+import com.employee.information.management.data.employee.dao.EmployeeDao;
+import com.employee.information.management.data.employee.dao.impl.EmployeeDaoImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeDaoImplTest {
+    @Mock
+    private EmployeeDao employeeDao;
+
+    @InjectMocks
+    private EmployeeDaoImpl employeeDaoImpl;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetAllEmployee() {
+
+        Employee employee1 = new Employee("Dela Cruz","Juan","B","Prof","May 1, 2020",
+                new Timestamp(System.currentTimeMillis()), "Maragondon","Male",
+                "Single","Filipino","Catholic", 149,43, "JuanB@gmail.com",
+                "123456", "123456", "123456", "01");
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee1);
+
+        EmployeeDao employeeDao = mock(EmployeeDao.class);
+        when(employeeDao.getAllEmployee()).thenReturn(employeeList);
+
+        List<Employee> expectedResult = employeeDao.getAllEmployee();
+
+        assertEquals(expectedResult.size(), 1);
+    }
+
+    @Test
+    public void testGetEmployeeByNo() {
+
+        Employee employee1 = new Employee("Dela Cruz","Juan","B","IT PROF","January 1",
+                new Timestamp(System.currentTimeMillis()), "Maragondon","Male",
+                "Single","Fil","Catholic", 149,43, "JuanB@gmail.com",
+                "12345", "12345", "12345", "01");
+
+        EmployeeDao employeeDao = mock(EmployeeDao.class);
+        when(employeeDao.getEmployeeByNo("1")).thenReturn(employee1);
+
+        Employee expectedEmployee = employeeDao.getEmployeeByNo("1");
+
+        assertEquals(expectedEmployee, employee1);
+    }
+
 }
