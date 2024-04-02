1f26790669e2a8d076ca59bf0c4189bb67ffede5
Martinez Conrad Loui <conradmartinez050@gmail.com>
2024-04-02 21:36:14 +0800

    RCPS-101 Employee Information Management: Create Documentation for EmployeeDao

--- src/main/java/com/employee/info/mgmt/data/employee/dao/EmployeeDao.java
+++ src/main/java/com/employee/info/mgmt/data/employee/dao/EmployeeDao.java
@@ -4,9 +4,31 @@ import com.employee.info.mgmt.appl.model.Employee;
 
 import java.util.List;
 
+/**
+ * The EmployeeDao interface manages employee data.
+ */
 public interface EmployeeDao {
+
+    /**
+     * Retrieves all employees.
+     *
+     * @return a List of all employees
+     */
     List<Employee> getAllEmployees();
+
+    /**
+     * Retrieves an employee by ID.
+     */
     Employee getEmployeeById(String id);
+
+    /**
+     * Adds a new employee.
+     */
     boolean addEmployee(Employee employee);
+
+    /**
+     * Updates an employee.
+     */
     boolean updateEmployee(Employee employee);
 }
+
