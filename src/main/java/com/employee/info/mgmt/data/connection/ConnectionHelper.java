7b54b40949c93fe649f91dad7a65bb9e1796c76b
unknown <julybayot26>
2024-04-04 00:26:03 +0800

    RCPS-104 Employee Information Management: Create Documentation and Implement Logging in ConnectionHelper

--- src/main/java/com/employee/info/mgmt/data/connection/ConnectionHelper.java
+++ src/main/java/com/employee/info/mgmt/data/connection/ConnectionHelper.java
@@ -2,19 +2,46 @@ package com.employee.info.mgmt.data.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ConnectionHelper {
+    private static final Logger logger = LogManager.getLogger(ConnectionHelper.class);
+
+    /**
+     * Database URL
+     */
     public static final String URL = "jdbc:oracle:thin:@localhost:1521:rogate";
+
+    /**
+     * Oracle JDBC Driver
+     */
     public static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
+
+    /**
+     * Database username
+     */
     public static final String username = "system";
+
+    /**
+     * Database password
+     */
     public static final String password = "Changeme0";
 
-    public static Connection getConnection()
-    {
+    /**
+     * Establishes a connection to the database.
+     *
+     * @return Connection object
+     * @throws RuntimeException if there is an error connecting to the database
+     */
+    public static Connection getConnection() {
         try {
             Class.forName(ORACLE_DRIVER).newInstance();
-            return DriverManager.getConnection(URL, username, password);
+            Connection connection = DriverManager.getConnection(URL, username, password);
+            logger.info("Connection established successfully");
+            return connection;
         } catch (Exception e) {
+            logger.error("Error connecting to the database", e);
             throw new RuntimeException("Error connecting to the database", e);
         }
     }
